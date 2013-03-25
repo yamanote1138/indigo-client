e95105577a085283958b4fab6bd466fe7ccdaae9
Chad Francis <discgolfer1138@gmail.com>
2013-03-24 22:52:28 -0600

    did a little refactoring, still getting the hang of this module business

---
+++ tests/test.js
@@ -0,0 +1,29 @@
+var indigo = require('./lib/indigo.js'),
+	prompt = require('prompt');
+
+prompt.start();
+
+var prompt_options = [
+	'host',
+	'user',
+	'pass',
+	'device_id',
+	{
+		'name':'key',
+		'default':'isOn'
+	},
+	{
+		'name':'value',
+		'default':1
+	}
+];
+
+prompt.get(prompt_options, function (err, result) {
+	var indigoClient = new indigo({host:result.host, user:result.user, pass:result.pass});
+
+	indigoClient.setDeviceValue( result.device_id, result.key, result.value, function(err, res, body){
+		if(err) console.log(err);
+		if(body) console.log(body);
+	});
+});
+
